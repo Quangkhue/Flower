73a785f77767f9ad6791f44120c186a9afe8ff31
Loc Nguyen <loc@vastbit.com>
2016-12-12 16:40:53 +0700

    Added angularjs and express

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+var app = angular.module("Flower", ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+      url: '/home',
+      templateUrl: 'home.html'
+  }).state('faq', {
+      url: "/faq",
+      templateUrl: 'faq.html'
+  });
+
+  $urlRouterProvider.otherwise('/home');
+});
---
+++ main.js
@@ -0,0 +1,4 @@
+'use strict';
+app.controller('MainCtrl', function($scope, $rootScope){
+    console.log("Main ctrl!");
+});
