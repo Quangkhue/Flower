31df329b69f4970c9787898f5c46d5ac58e9c761
quangkhue <quangkhue.ptit@gmail.com>
2016-12-16 10:24:39 +0700

    fix route viewss

--- client/app.js
+++ client/app.js
@@ -1,29 +1,34 @@
 'use strict';
 var app = angular.module("Flower", ['ui.router']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider.state('app', {
-      abstract: true,
-      views: {
-          root: {
-              templateUrl: "/layout/layout.html",
-              controller: "MainCtrl"
-          }
-      }
-  })
-  .state('app.home', {
-      templateUrl: "/modules/home/views/home.html",
-      controller: "HomeCtrl",
-      url: "/home"
-  })
-  .state('app.contact', {
-      templateUrl: "/modules/contact/views/contact.html",
-      url: "/contact"
-  })
-  .state('app.about', {
-      templateUrl: "/modules/about/views/about.html",
-      url: "/about"
-  });
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('app', {
+        abstract: true,
+        views: {
+            root: {
+                templateUrl: "/layout/layout.html",
+                controller: "MainCtrl"
+            }
+        }
+    })
+        .state('app.home', {
+            views: {
+                'contents': {
+                    templateUrl: "/modules/home/views/home.html",
+                    controller: "HomeCtrl",
+                },
+                 'sidebars': {}
+            },
+            url: "/home"
+        })
+        .state('app.contact', {
+            templateUrl: "/modules/contact/views/contact.html",
+            url: "/contact"
+        })
+        .state('app.about', {
+            templateUrl: "/modules/about/views/about.html",
+            url: "/about"
+        });
 
-  $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/home');
 });
