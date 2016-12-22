905289e58e7b9c4b51c5318617d6b86bea3be5f6
Loc Nguyen <loc@vastbit.com>
2016-12-22 14:08:01 +0700

    Fix bugs

--- client/app.js
+++ client/app.js
@@ -1,16 +1,16 @@
 'use strict';
 var app = angular.module("Flower", ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui.router']);
 
-app.config(function ($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('app', {
-        abstract: true,
-        views: {
-            root: {
-                templateUrl: "/layout/layout.html",
-                controller: "MainCtrl"
+            abstract: true,
+            views: {
+                root: {
+                    templateUrl: "/layout/layout.html",
+                    controller: "MainCtrl"
+                }
             }
-        }
-    })
+        })
         .state('app.home', {
             templateUrl: "/modules/home/views/home.html",
             controller: "HomeCtrl",
