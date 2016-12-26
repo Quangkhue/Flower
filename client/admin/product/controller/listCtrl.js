a4daf1302454fc0b161aac306da82af7be8873af
Loc Nguyen <loc@vastbit.com>
2016-12-26 16:09:47 +0700

    Added list product page

--- client/admin/product/controller/listCtrl.js
+++ client/admin/product/controller/listCtrl.js
@@ -1,5 +1,32 @@
 'use strict';
 
 app.controller("ProductListCtrl", function($scope, $rootScope, ProductSvc){
-    console.log("Product list controller");
+    $scope.init = function(){
+        ProductSvc.getProducts().then(function(result){
+            $scope.products = angular.copy(result);
+            console.log("Products: ", $scope.products);
+        });
+    }
+
+    $scope.edit = function(prod){
+        console.log("edit prod: ", prod);
+    }
+
+    $scope.delete = function(prodId, index){
+        console.log("Cat id: ", prodId);
+        ProductSvc.delete(prodId).then(function(res){
+            console.log("Delete product response: ", res);
+            $scope.products.splice(index, 1);
+        });
+    }
+
+    $scope.displayCatName = function(cats){
+        return cats.map(function(c){
+            return c.name
+        }).join(", ");
+    }
+
+    $scope.addNew = function(){
+        console.log("Add new product");
+    }
 });
